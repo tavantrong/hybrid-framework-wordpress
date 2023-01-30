e60a5db1abf83dd7803f379d7660120bc06c8f26
Trong Ta <tavantrong87@gmail.com>
2023-01-30 23:53:24 +0700

    update BaseTest

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -62,6 +62,50 @@ public class BaseTest {
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		return driver;
 	}
+	
+	protected WebDriver getBrowserDriver(String browserName) {
+		Browser browser = Browser.valueOf(browserName.toUpperCase());
+
+		if (browser == Browser.FIREFOX) {
+			// WebDriverManager.firefoxdriver().setup(); //Download hơi lâu nên xài manual
+			System.setProperty("webdriver.gecko.driver", projectLocator + "\\browserDriver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browser == Browser.CHROME) {
+			// System.setProperty("webdriver.chrome.driver", projectLocator +
+			// "\\browserDriver\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser == Browser.EDGE_CHROMIUM) {
+			// System.setProperty("webdriver.edge.driver", projectLocator +
+			// "\\browserDriver\\msedgedriver.exe");
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if (browser == Browser.OPERA) {
+			System.setProperty("webdriver.opera.driver", projectLocator + "\\browserDriver\\msedgedriver.exe");
+			// WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
+		} else if (browser == Browser.CHROME_HEADLESS) {
+			// System.setProperty("webdriver.edge.driver", projectLocator +
+			// "\\browserDriver\\msedgedriver.exe");
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions chromeOpt = new ChromeOptions();
+			chromeOpt.addArguments("headless");
+			chromeOpt.addArguments("window-size=1920x1080");
+			driver = new ChromeDriver(chromeOpt);
+		} else if (browser == Browser.FIREFOX_HEADLESS) {
+			// System.setProperty("webdriver.edge.driver", projectLocator +
+			// "\\browserDriver\\msedgedriver.exe");
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions firefoxOpt = new FirefoxOptions();
+			firefoxOpt.addArguments("-headless");
+			firefoxOpt.addArguments("window-size=1920x1080");
+			driver = new FirefoxDriver(firefoxOpt);
+		} else {
+			throw new RuntimeException("Please input the browser name!");
+		}
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		return driver;
+	}
 
 	public String getDirectorySlash(String folderName) {
 		if (isMac() || isLinux()) {
