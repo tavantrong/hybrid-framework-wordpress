cdcf623ae17620b57c598b12f98b011539daa493
Trong Ta <tavantrong87@gmail.com>
2023-02-03 09:09:50 +0700

    Data table handle

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -217,6 +217,10 @@ public class BasePage {
 		return getListWebElement(driver, locator).size();
 	}
 	
+	public int getElementNumber(WebDriver driver, String locator, String...values) {
+		return getListWebElement(driver, getDynamicLocator(locator, values)).size();
+	}
+	
 	public void checktoCheckbox(WebDriver driver, String locator) {
 		WebElement element = getWebElement(driver, locator);
 		if (!element.isSelected()) {
@@ -270,9 +274,14 @@ public class BasePage {
 		action.dragAndDrop(getWebElement(driver, sourceLocator), getWebElement(driver, targetLocator)).perform();
 	}
 	
-	public void sendkeyBoardToElement(WebDriver driver, String locator, Keys key) {
+	public void pressKeyboardToElement(WebDriver driver, String locator, Keys key) {
 		Actions action = new Actions (driver);
 		action.sendKeys(getWebElement(driver, locator), key).perform();
+	}
+	
+	public void pressKeyboardToElement(WebDriver driver, String locator, Keys key, String...values) {
+		Actions action = new Actions (driver);
+		action.sendKeys(getWebElement(driver, getDynamicLocator(locator, values)), key).perform();
 	}
 	
 	public Object executeForBrowser(WebDriver driver, String javaScript) {
---
+++ actions/pageObject/jQuery/HomePageObject.java
@@ -0,0 +1,53 @@
+package pageObject.jQuery;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import commons.BasePage;
+import pageUIs.jQuery.HomePageUI;
+
+
+public class HomePageObject extends BasePage {
+	private WebDriver driver;
+	
+	public HomePageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void openPageByNumber(String pageNumber) {
+		waitForElementClickable(driver, HomePageUI.HOME_PAGE_NUMBER, pageNumber);
+		clickToElement(driver, HomePageUI.HOME_PAGE_NUMBER, pageNumber);
+		
+	}
+
+	public boolean isPageActivebyNumber(String pageName) {
+		waitForElementVisible(driver, HomePageUI.HOME_PAGE_ACTIVE_BY_NUMBER, pageName);
+		return isElementDisplayed(driver, HomePageUI.HOME_PAGE_ACTIVE_BY_NUMBER, pageName);
+	}
+
+	public void inputToHeaderTextboxByLabel(String labelName, String value) {
+		waitForElementVisible(driver, HomePageUI.HEADER_TEXTBOX_BY_LABEL_NAME, labelName);
+		sendkeyToElement(driver, HomePageUI.HEADER_TEXTBOX_BY_LABEL_NAME, value, labelName);
+		pressKeyboardToElement(driver, HomePageUI.HEADER_TEXTBOX_BY_LABEL_NAME, Keys.ENTER, labelName);
+	}
+
+	public boolean areRowRecordDisplay(String females, String country, String males, String total) {
+		waitForElementVisible(driver, HomePageUI.ROW_VALUE_ALL_FIELD, females, country, males, total);
+		return isElementDisplayed(driver, HomePageUI.ROW_VALUE_ALL_FIELD, females, country, males, total);
+	}
+
+	public void clickToActionIconByContryName(String countryName, String actionName) {
+		waitForElementClickable(driver, HomePageUI.ACTION_ICON_BY_NAME, countryName, actionName);
+		clickToElement(driver, HomePageUI.ACTION_ICON_BY_NAME, countryName, actionName);
+		
+	}
+
+	public void inputToTextboxByColumnNameAtRowNumber(String columnName, String rowIndex, String value) {
+		waitForElementVisible(driver, HomePageUI.HEADER_NAME_PRECEDING, columnName);
+		// X - Column index
+		int columnIndex = getElementNumber(driver, HomePageUI.HEADER_NAME_PRECEDING, columnName) + 1;
+		// Y - Row index
+		sendkeyToElement(driver, HomePageUI.TEXTBOX_AT_COLUMN_AND_ROW_INDEX, value, rowIndex, String.valueOf(columnIndex));
+	}
+
+
+}
